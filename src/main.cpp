703afdb24321cf5734ea144bc272611feefcf256
Julien QUENTIN <jqntn88@gmail.com>
2025-11-29 23:08:19 +0100

    test rlsw

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <raylib.h>
+
+int
+main()
+{
+  SetConfigFlags(FLAG_WINDOW_TOPMOST);
+  InitWindow(320, 240, "birdfight");
+  SetExitKey(KEY_NULL);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RED);
+    DrawCircle(
+      GetScreenWidth() * 0.5f, GetScreenHeight() * 0.5f, 100.0f, GREEN);
+    EndDrawing();
+  }
+
+  CloseWindow();
+}
